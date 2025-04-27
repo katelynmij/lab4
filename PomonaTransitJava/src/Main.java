196a18d6ebe79ac71c04417ea92a83126e77ba2f
kennethhun <kenhun2020@gmail.com>
2025-04-26 19:47:17 -0700

    basic cli implemented maybe

--- PomonaTransitJava/src/Main.java
+++ PomonaTransitJava/src/Main.java
@@ -12,44 +12,114 @@ public class Main {
 
             Connection conn = DriverManager.getConnection(url);
             Scanner scanner = new Scanner(System.in);
+            int UserTask = 0;
+            
+            while (UserTask != 9) {
+                // basic menu
+                System.out.println("Enter a number between 1 and 9 for a given task");
+                System.out.println("1. Display the schedule of all trips for a given StartLocationName, Destination Name, and Date.");
+                System.out.println("2. Edit the schedule");
+                System.out.println("3. Display the stops of a given trip");
+                System.out.println("4. Display the weekly schedule of a given driver and date.");
+                System.out.println("5. Add a driver.");
+                System.out.println("6. Add a bus.");
+                System.out.println("7. Delete a bus.");
+                System.out.println("8. Record (insert) the actual data of a given trip offering specified by its key. ");
+                System.out.println("9. Exit the program");
+                UserTask = scanner.nextInt();
+                scanner.nextLine();
 
-            System.out.print("What is the start location?: ");
-            String start = scanner.nextLine();
+                switch(UserTask) {
+                    case 1:
+                        System.out.print("What is the start location?: ");
+                        String start = scanner.nextLine();
+    
+                        System.out.print("What is the destination?: ");
+                        String dest = scanner.nextLine();
+    
+                        System.out.print("What is the date? (yyyy-mm-dd): ");
+                        String dateInput = scanner.nextLine();
+    
+                        PreparedStatement ps = conn.prepareStatement(
+                            "SELECT t.TripNumber, o.Date, o.ScheduledStartTime, o.ScheduledArrivalTime, " +
+                            "o.DriverName, o.BusID " +
+                            "FROM Trip t JOIN TripOffering o ON t.TripNumber = o.TripNumber " +
+                            "WHERE t.StartLocationName = ? AND t.DestinationName = ? AND o.Date = ?"
+                        );
+    
+                        ps.setString(1, start);
+                        ps.setString(2, dest);
+                        ps.setDate(3, java.sql.Date.valueOf(dateInput));
+    
+                        ResultSet rs = ps.executeQuery();
+    
+                        System.out.println("\n-- Trip Schedule --");
+                        while (rs.next()) {
+                            System.out.println("Trip #: " + rs.getInt("TripNumber"));
+                            System.out.println("Date: " + rs.getDate("Date"));
+                            System.out.println("Start Time: " + rs.getTime("ScheduledStartTime"));
+                            System.out.println("Arrival Time: " + rs.getTime("ScheduledArrivalTime"));
+                            System.out.println("Driver: " + rs.getString("DriverName"));
+                            System.out.println("Bus ID: " + rs.getString("BusID"));
+                            System.out.println("-----");
+                            }
 
-            System.out.print("What is the destination?: ");
-            String dest = scanner.nextLine();
+                        rs.close();
+                        ps.close();
+                        break;
+                        
+                    case 2:
+                        // WIP
+                        break;
+                    case 3:
+                        // WIP
+                        break;
+                    case 4:
+                        // WIP
+                        break;
+                    case 5:
+                        // kenneth testing adding a driver
+                        System.out.println("What is the Driver's name: ");
+                        String name = scanner.nextLine();
 
-            System.out.print("What is the date? (yyyy-mm-dd): ");
-            String dateInput = scanner.nextLine();
+                        System.out.println("What is the Driver's phone number: (xxx-xxx-xxxx)");
+                        String phone = scanner.nextLine();
 
-            PreparedStatement ps = conn.prepareStatement(
-                "SELECT t.TripNumber, o.Date, o.ScheduledStartTime, o.ScheduledArrivalTime, " +
-                "o.DriverName, o.BusID " +
-                "FROM Trip t JOIN TripOffering o ON t.TripNumber = o.TripNumber " +
-                "WHERE t.StartLocationName = ? AND t.DestinationName = ? AND o.Date = ?"
-            );
+                        addDriver(conn, name, phone);
+                        break;
+                    case 6:
+                        // adding a bus
+                        System.out.println("Enter BusID: ");
+                        String BusID = scanner.nextLine();
 
-            ps.setString(1, start);
-            ps.setString(2, dest);
-            ps.setDate(3, java.sql.Date.valueOf(dateInput));
+                        System.out.println("Enter the bus model: ");
+                        String model = scanner.nextLine();
 
-            ResultSet rs = ps.executeQuery();
+                        System.out.println("Enter the bus: ");
+                        int year = scanner.nextInt();
+                        scanner.nextLine();
 
-            System.out.println("\n-- Trip Schedule --");
-            while (rs.next()) {
-                System.out.println("Trip #: " + rs.getInt("TripNumber"));
-                System.out.println("Date: " + rs.getDate("Date"));
-                System.out.println("Start Time: " + rs.getTime("ScheduledStartTime"));
-                System.out.println("Arrival Time: " + rs.getTime("ScheduledArrivalTime"));
-                System.out.println("Driver: " + rs.getString("DriverName"));
-                System.out.println("Bus ID: " + rs.getString("BusID"));
-                System.out.println("-----");
+                        addBus(conn, BusID, model, year);
+                        break;
+                    case 7:
+                        //deleting a bus
+                        System.out.println("Enter BusID: ");
+                        String busID = scanner.nextLine();
+
+                        deleteBus(conn, busID);
+                        break;
+                    case 8:
+                        //inserting actual trip information will add later
+                        break;
+                    case 9:
+                        System.out.println("Exiting");
+                        break;
+                    default:
+                        System.out.println("Please enter a valid value.");               
+                }
             }
+            
 
-
-
-            rs.close();
-            ps.close();
             conn.close();
             scanner.close();
         } catch (Exception e) {
